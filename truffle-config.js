abd7f4a4c59ab513facdb7b697d2217ad73eb40e
Javier Rodriguez <javrod83@gmail.com>
2021-05-26 00:05:07 -0300

    seed frase for testnet added

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,23 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic_privatekey = process.env["MNEMONIC_PRIVATEKEY"];
-
+//const mnemonic_privatekey = process.env["MNEMONIC_PRIVATEKEY"];
+const kargain_test_privatekey = "pilot potato spend dream mass genius tool advance spike artist patch swarm";
 module.exports = {
    networks: {
     local: {
-      provider: () => new HDWalletProvider(mnemonic_privatekey, "http://localhost"),
+      provider: () => new HDWalletProvider(kargain_test_privatekey, "http://localhost"),
       port: 7545,            // Standard BSC port (default: none)
       network_id: "5777",       // Any network (default: none)
 
     },
     testnet: {
-      provider: () => new HDWalletProvider(mnemonic_privatekey, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(kargain_test_privatekey, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
       skipDryRun: true
     },
     bsc: {
-      provider: () => new HDWalletProvider(mnemonic_privatekey, `https://bsc-dataseed1.binance.org`),
+      provider: () => new HDWalletProvider(kargain_test_privatekey, `https://bsc-dataseed1.binance.org`),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
