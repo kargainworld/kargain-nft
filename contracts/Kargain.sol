a85b6864472510ee4e7703ce1b34f44d711ae5b5
Roberto Sumoza <robertosumoza@gmail.com>
2021-06-03 15:09:15 -0400

    Refactorizacion del contrato.

--- contracts/Kargain.sol
+++ contracts/Kargain.sol
@@ -13,23 +13,22 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
     uint256 private constant COMMISSION_EXPONENT = 4;
     address payable private _platformAddress;
     uint256 private _platformCommissionPercent;
+    uint256 private _offerExpirationTime = 1 days;
 
-    mapping (uint => Token) private _tokens;
-    mapping (uint256 => address payable) private _offer_vendor;
-    mapping (uint256 => address payable) private _offers;
-    mapping (uint256 => bool) private _token_exist;
-    mapping (uint256 => bool) private _offer_claimed;
-    mapping (uint256 => uint256) private _offers_closeTimestamp;
+    mapping(uint256 => uint256) private _tokens_price;
+    mapping(uint256 => address payable) private _offers;
+    mapping(uint256 => uint256) private _offers_closeTimestamp;
 
     event TokenMinted(address indexed creator, uint256 indexed tokenId);
-    event OfferReceived(address indexed payer, uint tokenId);
-    event OfferAccepted(address indexed payer, uint tokenId);
-    event OfferRejected(address indexed payer, uint tokenId);
-
-    struct Token {
-        address owner;
-        uint amount;
-    }
+    event OfferReceived(
+        address indexed payer,
+        uint256 tokenId,
+        uint256 ammount
+    );
+    event OfferAccepted(address indexed payer, uint256 tokenId);
+    event OfferRejected(address indexed payer, uint256 tokenId);
+    event OfferExpired(address indexed payer, uint256 tokenId);
+    event OfferCancelled(address indexed payer, uint256 tokenId);
 
     struct TransferType {
         address from;
@@ -38,80 +37,221 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
         uint256 amount;
     }
 
-    modifier onlyAdmin(){
-        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Kargain: Caller is not a admin");
+    modifier onlyAdmin() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Kargain: Caller is not a admin"
+        );
         _;
     }
 
-    function initialize(address payable platformAddress_, uint256 platformCommissionPercent_)
-    initializer public {
-        _platformAddress = platformAddress_;
-        _platformCommissionPercent = platformCommissionPercent_;
+    modifier tokenExists(uint256 _tokenId) {
+        require(
+            _exists(_tokenId),
+            "Kargain: Id for this token does not exist."
+        );
+        _;
+    }
+
+    modifier onlyOwner(uint256 _tokenId) {
+        require(
+            ownerOf(_tokenId) == msg.sender,
+            "Kargain: You are not the owner of this token."
+        );
+        _;
+    }
+
+    modifier offerExist(uint256 _tokenId) {
+        require(
+            _offers[_tokenId] != address(0),
+            "Kargain: Does not exist any offer for this token."
+        );
+        _;
+    }
+
+    function initialize(
+        address payable _platformAddress_,
+        uint256 _platformCommissionPercent_
+    ) public initializer {
+        _platformAddress = _platformAddress_;
+        _platformCommissionPercent = _platformCommissionPercent_;
         __ERC721Burnable_init();
         __ERC721_init("Kargain", "KGN");
         __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function platformCommissionPercent() public view returns(uint256){
+    function platformCommissionPercent() public view returns (uint256) {
         return _platformCommissionPercent;
     }
 
-    function setPlatformCommissionPercent(uint256 _platformCommissionPercent_) public onlyAdmin {
-        _platformCommissionPercent = _platformCommissionPercent_;
+    function setPlatformCommissionPercent(uint256 platformCommissionPercent_)
+        public
+        onlyAdmin
+    {
+        _platformCommissionPercent = platformCommissionPercent_;
     }
 
-    function platformAddress() public view returns(address payable){
+    function platformAddress() public view returns (address payable) {
         return _platformAddress;
     }
 
-    function setPlatformAddress(address payable platformAddress_) public onlyAdmin {
+    function setPlatformAddress(address payable platformAddress_)
+        public
+        onlyAdmin
+    {
         _platformAddress = platformAddress_;
     }
 
-    function ownerOf(uint256 tokenId) public view override returns (address) {
-        address owner = _tokens[tokenId].owner;
-        require(owner != address(0), "ERC721: owner query for nonexistent token");
-        return owner;
+    function offerExpirationTime() public view returns (uint256) {
+        return _offerExpirationTime;
     }
 
-    function mint(uint256 tokenId) public payable{
-        require(!_token_exist[tokenId], "Kargain: Id for this token already exist");
-        super._mint(msg.sender, tokenId);
-        _tokens[tokenId].owner = msg.sender;
-        _offers[tokenId] = payable(address(0));
-        _offer_vendor[tokenId] = msg.sender;
-        _token_exist[tokenId] = true;
-        _offer_claimed[tokenId] = false;
-        _tokens[tokenId].amount = msg.value;
-        emit TokenMinted(msg.sender, tokenId);
+    function setOfferExpirationTime(uint256 offerExpirationTime_)
+        public
+        onlyAdmin
+    {
+        _offerExpirationTime = offerExpirationTime_;
     }
 
-    function purchaseToken(uint256 _tokenId) public payable {
-        require(_token_exist[_tokenId], "Kargain: Id for this token not exist");
-        require(!_offer_claimed[_tokenId], "Kargain: Offer was claimed");
-        require(_tokens[_tokenId].owner != msg.sender, "Kargain: You cannot buy your own token");
-        require(_offers[_tokenId] == address(0), "Kargain: An offer is pending");
-        require(msg.value == _tokens[_tokenId].amount, "Kargain: the offer amount is invalid");
-        _offers_closeTimestamp[_tokenId] = block.timestamp;
-        uint256 refundAmount = _tokens[_tokenId].amount;
-        _offers[_tokenId] = payable(address(msg.sender));
-        address payable refundAddress = _offers[_tokenId];
-        refundAddress.transfer(refundAmount);
-        emit OfferReceived(msg.sender, _tokenId);
+    function tokenPrice(uint256 _tokenId)
+        public
+        view
+        tokenExists(_tokenId)
+        returns (uint256)
+    {
+        return _tokens_price[_tokenId];
     }
 
-    function claimOffer(uint256 tokenId) public {
-        require(_token_exist[tokenId], "Kargain: Id for this token not exist");
-        require(!_offer_claimed[tokenId], "Kargain: Offer was claimed");
-        require(_offers_closeTimestamp[tokenId] < block.timestamp, "Kargain: Offer has expired");
-        _offer_claimed[tokenId] = true;
-        uint256 platformCommission;
-        platformCommission = (_tokens[tokenId].amount).mul(_platformCommissionPercent).div(10** COMMISSION_EXPONENT);
-        safeTransferFrom(address(this), _offer_vendor[tokenId], tokenId);
+    function setTokenPrice(uint256 _tokenId, uint256 _price)
+        public
+        onlyOwner(_tokenId)
+    {
+        _tokens_price[_tokenId] = _price;
+    }
+
+    function offerAddress(uint256 _tokenId)
+        public
+        view
+        tokenExists(_tokenId)
+        returns (address payable)
+    {
+        return _offers[_tokenId];
+    }
+
+    function _calculateCommission(uint256 price)
+        private
+        returns (uint256 commission)
+    {
+        return
+            price.mul(_platformCommissionPercent).div(10**COMMISSION_EXPONENT);
+    }
+
+    function _cancelOffer(uint256 _tokenId)
+        private
+        tokenExists(_tokenId)
+        offerExist(_tokenId)
+    {
+        delete _offers[_tokenId];
+        delete _offers_closeTimestamp[_tokenId];
+    }
+
+    function _refoundOffer(uint256 _tokenId)
+        private
+        tokenExists(_tokenId)
+        offerExist(_tokenId)
+    {
+        _offers[_tokenId].transfer(_tokens_price[_tokenId]);
+    }
+
+    function mint(uint256 _tokenId, uint256 _price) public {
+        require(
+            !_exists(_tokenId),
+            "Kargain: Id for this token already exists."
+        );
+        require(_price > 0, "Kargain: Prices must be greater than zero.");
+        super._mint(msg.sender, _tokenId);
+        _tokens_price[_tokenId] = _price;
+        emit TokenMinted(msg.sender, _tokenId);
+    }
+
+    function createOffer(uint256 _tokenId)
+        public
+        payable
+        tokenExists(_tokenId)
+    {
+        require(
+            ownerOf(_tokenId) != msg.sender,
+            "Kargain: You cannot buy your own token."
+        );
+        require(
+            _offers[_tokenId] == address(0),
+            "Kargain: An offer is pending."
+        );
+        require(
+            msg.value >= _tokens_price[_tokenId],
+            "Kargain: the offer amount is invalid."
+        );
+
+        _offers[_tokenId] = payable(msg.sender);
+        _offers_closeTimestamp[_tokenId] =
+            block.timestamp +
+            _offerExpirationTime;
+        emit OfferReceived(msg.sender, _tokenId, msg.value);
+    }
+
+    function acceptOffer(uint256 _tokenId)
+        public
+        tokenExists(_tokenId)
+        offerExist(_tokenId)
+        onlyOwner(_tokenId)
+    {
+        if (_offers_closeTimestamp[_tokenId] < block.timestamp) {
+            _cancelOffer(_tokenId);
+            emit OfferExpired(msg.sender, _tokenId);
+            return;
+        }
+
+        safeTransferFrom(msg.sender, _offers[_tokenId], _tokenId);
+
+        uint256 platformCommission =
+            _calculateCommission(_tokens_price[_tokenId]);
         _platformAddress.transfer(platformCommission);
-        _offer_vendor[tokenId].transfer((_tokens[tokenId].amount).sub(platformCommission));
-        _offer_vendor[tokenId] = _offers[tokenId];
+
+        msg.sender.transfer(_tokens_price[_tokenId].sub(platformCommission));
+
+        _cancelOffer(_tokenId);
+
+        emit OfferAccepted(msg.sender, _tokenId);
     }
 
+    function rejectOffer(uint256 _tokenId)
+        public
+        tokenExists(_tokenId)
+        onlyOwner(_tokenId)
+        offerExist(_tokenId)
+    {
+        _refoundOffer(_tokenId);
+        _cancelOffer(_tokenId);
+
+        emit OfferRejected(msg.sender, _tokenId);
+    }
+
+    function cancelOffer(uint256 _tokenId)
+        public
+        tokenExists(_tokenId)
+        offerExist(_tokenId)
+    {
+        require(
+            _offers[_tokenId] == msg.sender ||
+                hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Kargain: You do not have any offer for this token."
+        );
+
+        _refoundOffer(_tokenId);
+
+        _cancelOffer(_tokenId);
+
+        emit OfferCancelled(msg.sender, _tokenId);
+    }
 }
