5a4fb6a61877b865201859dfaf5644304f0dcea9
Damian <damianclluch@gmail.com>
2021-06-10 10:53:32 +0200

    refactor

--- contracts/Kargain.sol
+++ contracts/Kargain.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity >=0.6.0 <0.8.0;
 
-import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/ERC721BurnableUpgradeable.sol";
-import "../node_modules/@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "../node_modules/@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
-import "../node_modules/@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 
 contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
     using SafeMathUpgradeable for uint256;
@@ -22,15 +20,18 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
     mapping(uint256 => uint256) private _offers_closeTimestamp;
 
     event TokenMinted(address indexed creator, uint256 indexed tokenId);
-    event OfferReceived(
-        address indexed payer,
-        uint256 tokenId,
-        uint256 amount
-    );
+    event OfferReceived(address indexed payer, uint256 tokenId, uint256 amount);
     event OfferAccepted(address indexed payer, uint256 tokenId);
     event OfferRejected(address indexed payer, uint256 tokenId);
     event OfferExpired(address indexed payer, uint256 tokenId);
     event OfferCancelled(address indexed payer, uint256 tokenId);
+
+    struct TransferType {
+        address from;
+        address to;
+        uint256 tokenId;
+        uint256 amount;
+    }
 
     modifier onlyAdmin() {
         require(
@@ -76,90 +77,71 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function platformCommissionPercent() public view returns (uint256) {
+    function platformCommissionPercent() external view returns (uint256) {
         return _platformCommissionPercent;
     }
 
     function setPlatformCommissionPercent(uint256 platformCommissionPercent_)
-        public
-        onlyAdmin
+    external
+    onlyAdmin
     {
         _platformCommissionPercent = platformCommissionPercent_;
     }
 
-    function platformAddress() public view returns (address payable) {
+    function platformAddress() external view returns (address payable) {
         return _platformAddress;
     }
 
     function setPlatformAddress(address payable platformAddress_)
-        public
-        onlyAdmin
+    external
+    onlyAdmin
     {
         _platformAddress = platformAddress_;
     }
 
-    function offerExpirationTime() public view returns (uint256) {
+    function offerExpirationTime() external view returns (uint256) {
         return _offerExpirationTime;
     }
 
     function setOfferExpirationTime(uint256 offerExpirationTime_)
-        public
-        onlyAdmin
+    external
+    onlyAdmin
     {
         _offerExpirationTime = offerExpirationTime_;
     }
 
     function tokenPrice(uint256 _tokenId)
-        public
-        view
-        tokenExists(_tokenId)
-        returns (uint256)
+    external
+    view
+    tokenExists(_tokenId)
+    returns (uint256)
     {
         return _tokens_price[_tokenId];
     }
 
-    function setTokenPrice(uint256 _tokenId, uint256 _price)
-        public
-        onlyOwner(_tokenId)
-    {
-        _tokens_price[_tokenId] = _price;
-    }
-
     function offerAddress(uint256 _tokenId)
-        public
-        view
-        tokenExists(_tokenId)
-        returns (address payable)
+    external
+    view
+    tokenExists(_tokenId)
+    returns (address payable)
     {
         return _offers[_tokenId];
     }
 
-    function _calculateCommission(uint256 price)
-        private view
-        returns (uint256 commission)
+    function _refoundOffer(uint256 _tokenId)
+    private
+    tokenExists(_tokenId)
+    offerExist(_tokenId)
     {
-        return
-            price.mul(_platformCommissionPercent).div(10**COMMISSION_EXPONENT);
-    }
+        address payable buyer = _offers[_tokenId];
 
-    function _cancelOffer(uint256 _tokenId)
-        private
-        tokenExists(_tokenId)
-        offerExist(_tokenId)
-    {
         delete _offers[_tokenId];
         delete _offers_closeTimestamp[_tokenId];
+
+        buyer.transfer(_tokens_price[_tokenId]);
     }
 
-    function _refundOffer(uint256 _tokenId)
-        private
-        tokenExists(_tokenId)
-        offerExist(_tokenId)
-    {
-        _offers[_tokenId].transfer(_tokens_price[_tokenId]);
-    }
-
-    function mint(uint256 _tokenId, uint256 _price) public {
+    function mint(uint256 _tokenId, uint256 _price) external {
         require(
             !_exists(_tokenId),
             "Kargain: Id for this token already exists."
@@ -171,17 +153,17 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
     }
 
     function createOffer(uint256 _tokenId)
-        public
-        payable
-        tokenExists(_tokenId)
+    external
+    payable
+    tokenExists(_tokenId)
     {
         require(
             ownerOf(_tokenId) != msg.sender,
             "Kargain: You cannot buy your own token."
         );
         require(
-            _offers[_tokenId] == address(0),
-            "Kargain: An offer is pending."
+            _offers[_tokenId] == address(0) && _offers_closeTimestamp[_tokenId] < block.timestamp,
+            "Kargain: An offer is already submitted."
         );
         require(
             msg.value == _tokens_price[_tokenId],
@@ -189,58 +171,64 @@ contract Kargain is ERC721BurnableUpgradeable, AccessControlUpgradeable {
         );
 
         _offers[_tokenId] = payable(msg.sender);
-        _offers_closeTimestamp[_tokenId] = _offerExpirationTime.add(now);
+        _offers_closeTimestamp[_tokenId] =
+        block.timestamp +
+        _offerExpirationTime;
         emit OfferReceived(msg.sender, _tokenId, msg.value);
     }
 
     function acceptOffer(uint256 _tokenId)
-    public
+    external
     tokenExists(_tokenId)
     offerExist(_tokenId)
     onlyOwner(_tokenId)
     {
         if (_offers_closeTimestamp[_tokenId] < block.timestamp) {
-            _cancelOffer(_tokenId);
+            delete _offers[_tokenId];
+            delete _offers_closeTimestamp[_tokenId];
             emit OfferExpired(msg.sender, _tokenId);
             return;
         }
+        address payable buyer = _offers[_tokenId];
 
+        delete _offers[_tokenId];
+        delete _offers_closeTimestamp[_tokenId];
+
+        safeTransferFrom(msg.sender, buyer, _tokenId);
 
         uint256 platformCommission =
-        _calculateCommission(_tokens_price[_tokenId]);
+        _tokens_price[_tokenId].mul(_platformCommissionPercent).div(
+            10**COMMISSION_EXPONENT
+        );
         _platformAddress.transfer(platformCommission);
+
         msg.sender.transfer(_tokens_price[_tokenId].sub(platformCommission));
-        safeTransferFrom(msg.sender, _offers[_tokenId], _tokenId);
-        _cancelOffer(_tokenId);
 
         emit OfferAccepted(msg.sender, _tokenId);
     }
 
     function rejectOffer(uint256 _tokenId)
-        public
-        tokenExists(_tokenId)
-        onlyOwner(_tokenId)
-        offerExist(_tokenId)
+    external
+    tokenExists(_tokenId)
+    onlyOwner(_tokenId)
+    offerExist(_tokenId)
     {
-        _refundOffer(_tokenId);
-        _cancelOffer(_tokenId);
+        _refoundOffer(_tokenId);
 
         emit OfferRejected(msg.sender, _tokenId);
     }
 
     function cancelOffer(uint256 _tokenId)
-        public
-        tokenExists(_tokenId)
-        offerExist(_tokenId)
+    external
+    tokenExists(_tokenId)
+    offerExist(_tokenId)
     {
         require(
             _offers[_tokenId] == msg.sender ||
-                hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
             "Kargain: You do not have any offer for this token."
         );
-
-        _refundOffer(_tokenId);
-        _cancelOffer(_tokenId);
+        _refoundOffer(_tokenId);
 
         emit OfferCancelled(msg.sender, _tokenId);
     }
