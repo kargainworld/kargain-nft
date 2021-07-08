4aa4762452edf11783f572c1398a357b2d78ca0d
Damian <damianclluch@gmail.com>
2021-07-08 23:06:56 +0200

    wip

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Kargain = artifacts.require("Kargain");
 
-module.exports = function (deployer) {
+module.exports = async (deployer, network, accounts) => {
   deployer.deploy(Kargain);
+  const instance = await Kargain.deployed();
+  await instance.initialize(accounts[0], 10)
 };
