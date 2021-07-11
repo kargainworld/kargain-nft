1abe614c7149a75837976c71bca761957df0a4f6
Christian Escalante <chescalante.ar@gmail.com>
2021-07-11 18:41:34 -0300

    migrate changes

--- migrations/2_deploy_proxy.js
+++
@@ -1,9 +0,0 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-const Kargainv01 = artifacts.require('Kargainv01');
-
-module.exports = async function (deployer) {
-  const instance = await deployProxy(Kargainv01, { deployer, initializer: false});
-  console.log('Proxy Deployed', instance.address);
-  const output = await instance.version();
-  console.log('Version', output );
-}
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,14 @@
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const mnemonicPrivateKey = process.env["MNEMONIC_PRIVATEKEY"];
 const mnemonicPrivateKeyTesnet = process.env["MNEMONIC_PRIVATEKEY_TESNET"];
-require('dotenv').config();
-
 
 module.exports = {
    networks: {
     local: {
-      provider: () => new HDWalletProvider(mnemonicPrivateKey, "http://localhost"),
+      provider: () => new HDWalletProvider(mnemonicPrivateKey, "http://localhost:7545"),
       port: 7545,            // Standard BSC port (default: none)
       network_id: "5777",       // Any network (default: none)
-
     },
     testnet: {
       provider: () => new HDWalletProvider(mnemonicPrivateKeyTesnet, `https://data-seed-prebsc-1-s1.binance.org:8545`),
